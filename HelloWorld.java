512db2ffc03a1881631c3821779223e11d7d7108
Eoin <G00363134@gmit.ie>
2019-02-26 17:15:25 +0000

    +prototype, wordCloud must be redesigned

--- HelloWorld.java
+++ HelloWorld.java
@@ -11,12 +11,30 @@ public class HelloWorld{
         //menu.frequencyTable = menu.getShortTable();
         menu.frequencyTable = menu.sortTable();
         menu.frequencyTable = menu.getShortTable();
-        menu.displayTable();
+        //menu.displayTable();
 
         //System.out.println(menu.getIgnoreList());
+        //used to determine which % the word should fall into
+        //eg: index 0 is highest, will be assigned size/color to match
+        int i = 0;
+        int factor = 5;
+        float maxWordCount = 0;
+        float fWordValue;
+        int iWordValue;
 
         for (Map.Entry<String, Integer> ft: menu.frequencyTable.entrySet()){
-            wcg = new WordCloudCreator(ft.getKey());
+            if (i == 0) maxWordCount = ft.getValue();
+            //wordValue = ((maxWordCount - ft.getValue()) - maxWordCount);// % 10 * 2;
+            fWordValue = (ft.getValue() / maxWordCount) * (factor / 1);
+            if (fWordValue < factor - 0.5) { //seems to be a decent place to split it, can be changed.  Looks reasonable for 20, 100, 1000
+                fWordValue = --factor;
+                maxWordCount = ft.getValue();
+            }
+            iWordValue = Math.round(fWordValue);
+            //System.out.println(i++ + ": " + iWordValue);
+            i++;
+
+            wcg = new WordCloudCreator(ft.getKey(), iWordValue);
         }
 
         WordCloudCreator.saveImg(menu.fileOut);
--- WordCloudCreator.java
+++ WordCloudCreator.java
@@ -3,27 +3,106 @@ import java.awt.image.*;
 import javax.imageio.*;
 import java.io.*;
 
+/*
+I'm the creator of Wordle. Here's how Wordle actually works:
+
+Count the words, throw away boring words, and sort by the count, descending.
+Keep the top N words for some N. Assign each word a font size proportional to its count.
+Generate a Java2D Shape for each word, using the Java2D API.
+
+Each word "wants" to be somewhere, such as "at some random x position in the vertical center".
+I decreasing order of frequency, do this for each word:
+
+place the word where it wants to be
+while it intersects any of the previously placed words
+    move it one step along an ever-increasing spiral
+
+That's it. The hard part is in doing the intersection-testing efficiently,
+for which I use last-hit caching, hierarchical bounding boxes,
+and a quadtree spatial index (all of which are things you can learn more about with some diligent googling).
+
+Edit: As Reto Aebersold pointed out, there's now a book chapter, freely available,
+that covers this same territory: Beautiful Visualization, Chapter 3: Wordle
+
+
+*/
 public class WordCloudCreator {   
-    static BufferedImage image= new BufferedImage(600, 300, BufferedImage.TYPE_4BYTE_ABGR);
-    //Font font = new Font
-    //G
+    static int[] wordSizes = {12, 22, 32, 42, 52, 62};
+    final static int sizeX = 600;
+    final static int sizeY = 300;
+    static int x = (sizeX - wordSizes[wordSizes.length-1]) / 2;   //indicates start location
+    static int y = sizeY / 2;
+    static BufferedImage image= new BufferedImage(sizeX, sizeY, BufferedImage.TYPE_4BYTE_ABGR);
+    private static java.util.Random rnd = new java.util.Random();
+    //must create a function to modify x and y
+    //must also check for collision
+    //must know the height of words
 
     Font font;
     static Graphics graphics = image.getGraphics();
+    
        
     //constructor
-    WordCloudCreator(String word)
+    WordCloudCreator(String word, int wordValue)
     {
         //below inputs are for test purposes, all of the text is currently on top of each other
         //unsure how to ensure the text is correctly positioned
         //how to handle the change of size and color?
         //if I know number of numbers to be displayed then it could be % based, would require a size to be set which can be done statically
         //or passed when a word is created
-        font = new Font(Font.SANS_SERIF, Font.BOLD, 62);    //to be changed
-        graphics.setColor(Color.red);                       //to be changed
+        font = createFont(word, wordValue); 
 		graphics.setFont(font);
-        graphics.drawString(word, 0, 100);                  //to be changed
-        
+        graphics.drawString(word, x, y);                  //to be changed
+        //System.out.println(word + " x: " + x + " y: " + y);
+        //create new function: drawFont: Calls drawString() and implements algorithm to position(ensure no overlapping)
+        //y += 20;
+    }
+
+    private static void getWordLocation(int wordSize)
+    {
+        //should be called after createFont (within this function) - and use the previous words height/width to determine next location
+        //following is simplistic, not correct
+    
+        x = rnd.nextInt(sizeX - (100 - wordSize));
+        y = rnd.nextInt(sizeY - 50);
+        y += 50;
+
+    }
+    
+    private Font createFont(String word, int wordValue)
+    {
+        Font font = null;
+        //System.out.println(wordValue);
+
+        switch (wordValue){
+            case 5:
+                font = new Font(Font.SANS_SERIF, Font.BOLD, wordSizes[wordValue]);
+                graphics.setColor(Color.red);
+                break;
+            case 4:
+                font = new Font(Font.SANS_SERIF, Font.BOLD, wordSizes[wordValue]);
+                graphics.setColor(Color.orange);
+                break;
+            case 3:
+                font = new Font(Font.SANS_SERIF, Font.ITALIC, wordSizes[wordValue]);
+                graphics.setColor(Color.blue);
+                break;
+            case 2:
+                font = new Font(Font.SANS_SERIF, Font.ITALIC, wordSizes[wordValue]);
+                graphics.setColor(Color.yellow);
+                break;
+            case 1:
+                font = new Font(Font.SANS_SERIF, Font.ITALIC, wordSizes[wordValue]);
+                graphics.setColor(Color.cyan);
+                break;
+            default:
+                //do not reach 
+                font = new Font(Font.MONOSPACED, Font.PLAIN, wordSizes[0]);
+                graphics.setColor(Color.green);
+                break;
+        }
+        getWordLocation(wordSizes[wordValue]);
+        return font;
     }
 
     public static void dispose()
